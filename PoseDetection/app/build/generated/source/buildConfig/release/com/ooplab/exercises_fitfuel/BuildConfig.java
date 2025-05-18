c774ae9c8b0f0b4bf41f14b6589a2a780088371e
yunyesung <bbb6973@naver.com>
2025-05-18 18:04:59 +0900

    feat: remove visualization and switch to lite graph

---
+++ PoseDetection/app/build/generated/source/buildConfig/release/com/ooplab/exercises_fitfuel/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ooplab.exercises_fitfuel;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.ooplab.exercises_fitfuel";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
