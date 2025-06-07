7b79ffe8794c55d289c803967b01fd96f24535c7
whiskend <lsg008@naver.com>
2025-06-07 16:53:43 +0900

    Add initial UI screens

--- app/src/main/java/com/ooplab/exercises_fitfuel/MainScreenActivity.kt
+++ app/src/main/java/com/ooplab/exercises_fitfuel/MainScreenActivity.kt
@@ -1,24 +1,23 @@
 package com.ooplab.exercises_fitfuel
 
-import android.os.Bundle
 import android.content.Intent
+import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
 
 class MainScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mainscreen)
 
-        val moveMainActivityButton: Button = findViewById(R.id.moveMainActivityButton)
-        moveMainActivityButton.setOnClickListener {
+        val btnChallenge = findViewById<Button>(R.id.btnChallengeMode)
+        btnChallenge.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
-        val moveMyPageButton = findViewById<Button>(R.id.moveMyPageButton)
-        moveMyPageButton.setOnClickListener {
+        val btnPractice = findViewById<Button>(R.id.btnPracticeMode)
+        btnPractice.setOnClickListener {
             val intent = Intent(this, MyPageActivity::class.java)
             startActivity(intent)
         }
